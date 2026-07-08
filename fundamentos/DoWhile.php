5b58db6ea4272cce115d2fdc8e6d50100631f1aa
Guilherme Varjão <guilhermmesantos98@gmail.com>
2026-07-07 22:21:06 -0300

    Do While, For e Foreach

--- fundamentos/DoWhile.php
+++ fundamentos/DoWhile.php
@@ -6,10 +6,22 @@
     <title>Laço de repetição Do...While</title>
 </head>
 <body>
-    
+    <?php
+//Capacidade máxima da garrafa
+
+$capacidade_garrafa = 450; //ml
+$agua_na_garrafa = 0; //ml
+$quantidade_por_dose = 50; //ml
+
+do {
+    $agua_na_garrafa += $quantidade_por_dose;
+    echo "Quantidade de água na garrafa: $agua_na_garrafa ml <br>";
+} while ($agua_na_garrafa < $capacidade_garrafa);
 
 
 
+
+?>
 
 
 </body>
