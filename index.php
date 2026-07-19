d29dc47651a3cc2d739f1509cff77c3985c63e45
Guilherme Varjão <guilhermmesantos98@gmail.com>
2026-07-19 10:02:45 -0300

    seguindo na conexão com o DB

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
-<?php 
 
+<?php
+
+
+// incluir o arquivo com a conexão com o banco de dados
 require_once "./connection.php";
 
-//colocacando dados na tabela users
+
+/***************************************************/
+/******* colocacando dados diretos na QUERY ********/
+/***************************************************/
+
+//Criar a QUERY para inserir dados no banco de dados(CADASTRO DE USUARIOS)
 //$sql = " INSERT INTO users (name, email) VALUES ('Giovanna', 'giovanna@gmail.com')";
 
 //Prepara a query para ser executada no banco de dados
@@ -11,13 +19,29 @@ require_once "./connection.php";
 //Executa a query no banco de dados
 //$stmt->execute();               
 
+  /***********************************************************/
+ /******* Usar link na QUERY  e substituir por binParam *****/
+/***********************************************************/
 
-$name = "Murilo";
-$email = "murilo@gmail.com";
+// $name = "Murilo";
+// $email = "murilo@gmail.com";
 
+//Colocar valores direto na query
+// $sql = "INSERT INTO users (name, email) Values (:name, :email)";
 
-$sql = "INSERT INTO users (name, email) Values (:name, :email)";
 //Prepara a query 
-$stmt = $conn->prepare($sql);
-//Executa a query 
-$stmt->execute();                 
+// $stmt = $conn->prepare($sql);
+
+//Substituir os links da QUERY pelos valores das variáveis
+// $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+// $stmt->bindParam(':email',$email, PDO::PARAM_STR);
+
+//executa a query no banco de dados  
+// $stmt->execute();   
+
+  /************************************************************/
+ /******* Usar link na QUERY  e substituir por bindValue *****/
+/************************************************************/
+
+
+echo "<p style='color: #086;'>executou todos os comandos!</p>";
