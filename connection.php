3717d7d22abef46ff83598824b0a1c59bb3382d7
Guilherme Varjão <guilhermmesantos98@gmail.com>
2026-07-23 20:44:14 -0300

    criando primeiro formulário

--- connection.php
+++ connection.php
@@ -9,7 +9,7 @@ $port = 3306;
 try {
 
     $conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
-    echo "Conexão com o banco de dados realizado com sucesso!";
+    //echo "Conexão com o banco de dados realizado com sucesso!";
 } catch (PDOException $e) {
     //echo "Erro: Conexão com o banco de dados não realizado. Erro gerado " . $e->getMessage();
     die("Erro 001: Por favor tente novamente mais tarde. Caso o problema persista, entre em contato com o administrador guilhermmesantos98@gmail.com");
--- index.php
+++ index.php
@@ -1,80 +1,38 @@
-
 <?php
+//Inclui arquivo com a conexão com banco de dados
+require_once('./connection.php');
+?>
 
+<!DOCTYPE html>
+<html lang="pt-br">
 
-// incluir o arquivo com a conexão com o banco de dados
-require_once "./connection.php";
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Celke</title>
+</head>
 
+<body>
 
-/***************************************************/
-/******* colocacando dados diretos na QUERY ********/
-/***************************************************/
+  <form method="POST" action="">
 
-//Criar a QUERY para inserir dados no banco de dados(CADASTRO DE USUARIOS)
-//$sql = " INSERT INTO users (name, email) VALUES ('Giovanna', 'giovanna@gmail.com')";
+      
+      
+      <input type="hidden" name="csrf_token" value="123456">
 
-//Prepara a query para ser executada no banco de dados
-//$stmt = $conn->prepare($sql);
+      <label>nome</label> 
+      <input type="text" name="name" placeholder="nome completo" required>
+      <br><br>
 
+      <label>email</label>
+      <input type="email" name="email" id="email" required>
+      <br><br>
 
-//Executa a query no banco de dados
-//$stmt->execute();               
-
-  /***********************************************************/
- /******* Usar link na QUERY  e substituir por binParam *****/
-/***********************************************************/
-
-// $name = "Murilo";
-// $email = "murilo@gmail.com";
-
-//Colocar valores direto na query
-// $sql = "INSERT INTO users (name, email) Values (:name, :email)";
-
-//Prepara a query 
-// $stmt = $conn->prepare($sql);
-
-//Substituir os links da QUERY pelos valores das variáveis
-// $stmt->bindParam(':name', $name, PDO::PARAM_STR);
-// $stmt->bindParam(':email',$email, PDO::PARAM_STR);
-
-//executa a query no banco de dados  
-// $stmt->execute();   
-
-  /************************************************************/
- /******* Usar link na QUERY  e substituir por bindValue *****/
-/************************************************************/
-
-//Criar a QUERY cadastrar usuario
-$sql = " INSERT INTO users (name, email) VALUES (:name, :email)";
-
-// Preparar a query
-$stmt = $conn->prepare($sql);
-
-//substituir os links da QUERY pelos valores 
-$stmt->bindValue(':name', 'João', PDO::PARAM_STR);
-$stmt->bindValue(':email', 'joao@gmail.com', PDO::PARAM_STR);
-
-//executa a query 
-$stmt->execute();
+      <input type="submit" value="Cadastrar">
 
 
 
-   /************************************************************/
-  /******* Usar link na QUERY  e substituir por bindValue *****/
- /***** Instrução preparada com ponto de interrigação (?) ****/
-/***********************************************************/
 
-//Criar a QUERY cadastrar usuario
-//$sql = " INSERT INTO users (name, email) VALUES (?, ?)";
+</body>
 
-// Preparar a query
-//$stmt = $conn->prepare($sql);
-
-//substituir os links da QUERY pelos valores 
-//$stmt->bindValue(1, 'Ferdinando', PDO::PARAM_STR);
-//$stmt->bindValue(2, 'ferdinando@gmail.com', PDO::PARAM_STR);
-
-//executa a query 
-//$stmt->execute();
-
-echo "<p style='color: #086;'>executou todos os comandos!</p>";
+</html>
